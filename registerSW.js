6d62117576fa27ade33157a7e41c8526abb60f25
Myles OReilly <oreillym7846-web@users.noreply.github.com>
2026-08-14 19:04:13 -0600

    Promote Live the Language v0.7.4

--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/LearningAnotherLanguageOS-App/sw.js', { scope: '/LearningAnotherLanguageOS-App/' })})}
